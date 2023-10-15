6d95af79a06cbe8a7def85c8b6dbca8308170c9b
yosriksonti <yosri.kossontini@esprit.tn>
2023-10-15 16:48:18 +0100

    log client ip

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -98,7 +98,7 @@ class ApplicationController extends AbstractController
             ->getRepository(Application::class)
             ->findOneBy(['secret' => $secret]);
         $this->user = $usr;
-        $response = new Response(json_encode(array('active' => $application->getActive(), 'request' => $request->getUri())));
+        $response = new Response(json_encode(array('active' => $application->getActive(), 'request' => $request->getClientIp())));
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
